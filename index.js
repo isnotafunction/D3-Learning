434f3c3819502926bcf1217b639af55338ecc40a
isnotafunction <ivana.cosgrove@gmail.com>
2019-01-27 20:08:29 +0000

    using data() and enter() to loop over a dataset and create new items

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-//select multiple elements
-d3.selectAll('li').text('list item')
+//working with data() and enter()
+//a bit counter-intuitive to be selecting elements
+//that don't exist yet
+
+const dataset = [1, 2, 3, 4, 5]
+
+d3.select('ul')
+  .selectAll('li')//there will be only 2
+  .data(dataset)//reviews the dataset
+  .enter()//sees there are 2 el on the page but needs 5
+  .append('li')//new li elements are appended
+  .text('new item')
