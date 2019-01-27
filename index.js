1f2073919e2dbd1deab5ceaba8182d980e978a22
isnotafunction <ivana.cosgrove@gmail.com>
2019-01-27 21:11:47 +0000

    dynamically set the coordinates for each bar

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const dataset = [1, 2, 3, 4, 5]
 const w = 500
 const h = 200
 
-d3.select('body')
+const svg = d3.select('body')
   .append('svg')
   .attr('width', w)
   .attr('height', h)
+
+svg.selectAll('rect')
+  .data(dataset)
+  .enter()
   .append('rect')
   .attr('width', 25)
   .attr('height', 100)
-  .attr('x', 0)
+  .attr('x', (d, i) => {
+    return i * 30
+  })
   .attr('y', 0)
