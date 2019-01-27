468ca7770d19e6cdaad72623ff6b970e95b4c0d9
isnotafunction <ivana.cosgrove@gmail.com>
2019-01-27 20:18:11 +0000

    change styles based on data providing a callback in the style method

--- index.js
+++ index.js
@@ -10,4 +10,10 @@ d3.select('ul')
   .enter()//sees there are 2 el on the page but needs 5
   .append('li')//new li elements are appended
   .text(d => d)//text takes a callback
-  .style('color', 'tomato')
+  .style('color', d => {
+    if (d > 4) {
+      return 'teal'
+    } else {
+      return 'tomato'
+    }
+  })
