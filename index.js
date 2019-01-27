94bb812069c776fceddc848307444c7d1cba3123
isnotafunction <ivana.cosgrove@gmail.com>
2019-01-27 20:56:39 +0000

    create svg area to place shapes in

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 
 const dataset = [1, 2, 3, 4, 5]
+const w = 500
+const h = 200
 
 d3.select('body')
-  .selectAll('div')
-  .data(dataset)
-  .enter()
-  .append('div')
-  .attr('class', 'bar')
-  .style('height', d => {
-    return d * 20 + 'px'
-  })
+  .append('svg')
+  .attr('width', w)
+  .attr('height', h)
