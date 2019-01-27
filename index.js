d820cb6139c2678fb5bd52cdd9f1664d18b3f25c
isnotafunction <ivana.cosgrove@gmail.com>
2019-01-27 20:41:23 +0000

    update height of an element dynamically

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-//working with data() and enter()
-//a bit counter-intuitive to be selecting elements
-//that don't exist yet
 
 const dataset = [1, 2, 3, 4, 5]
 
-d3.select('ul')
-  .selectAll('li')//there will be only 2
-  .data(dataset)//reviews the dataset
-  .enter()//sees there are 2 el on the page but needs 5
-  .append('li')//new li elements are appended
-  .text(d => d)//text takes a callback
-  .attr('class', d => {
-    if (d > 4) {
-      return 'high'
-    } else {
-      return 'low'
-    }
+d3.select('body')
+  .selectAll('div')
+  .data(dataset)
+  .enter()
+  .append('div')
+  .attr('class', 'container')
+  .style('height', d => {
+    return d * 10 + 'px'
   })
